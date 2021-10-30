f5e30b59a0126f4f146381034cbce1e6c33a93d5
Karepanova <89221006233@yandex.ru>
2021-10-30 22:27:46 +0500

    15 спринт - итог 2.1

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -1,15 +1,6 @@
-const { isCelebrateError } = require('celebrate');
-const IncorrectDataError = require('../errors/incorrect-data-err');
-
 module.exports = ((err, req, res, next) => {
-  let { statusCode = 500, message } = err;
-  if (isCelebrateError(err)) {
-    if (!err.details.get('body')) {
-      ({ statusCode, message } = new IncorrectDataError(err.details.get('params').message));
-    } else {
-      ({ statusCode, message } = new IncorrectDataError(err.details.get('body').message));
-    }
-  }
+  const { statusCode = 500, message } = err;
+
   res.status(statusCode).send({
     message: statusCode === 500
       ? 'На сервере ошибка'
